135aca7536f099ee85e5dd809f8b8c89ae8df093
Scott Cormier <chicormier@gmail.com>
2023-10-13 19:09:46 -0500

    console now shows each quote as a paragraph

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-const btn = document.querySelector("button");
-const output = document.querySelector("#output");
+const btn = document.querySelector('button')
+const output = document.querySelector('#output')
 
-btn.addEventListener("click",getQuote)
-function getQuote(){
-    const xhr = new XMLHttpRequest();
-    const url = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";
-    xhr.onreadystatechange = function(){
-        // console.log(xhr.readyState);
-        if(xhr.readyState == 4 && xhr.status == 200){
-            // console.log(xhr.responseText);
-            let str = xhr.responseText;
-            let obj = JSON.parse(str);
-            // console.log(obj);
+btn.addEventListener('click', getQuote)
+function getQuote() {
+  const xhr = new XMLHttpRequest()
+  const url = 'https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand'
+  xhr.onreadystatechange = function () {
+    // console.log(xhr.readyState);
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // console.log(xhr.responseText);
+      let str = xhr.responseText
+      let obj = JSON.parse(str)
+      //   console.log(obj['3']['content'])
 
-            obj.forEach((arrayItem)=>{
-                let x = arrayItem.content;
-                console.log(x);
-                let str1 = JSON.stringify(x);
-                str1 = str1.slice(13, -22);
-                output.innerHTML = str1;
-            });
-        }else{
-            output.innerHTML = "Error";
-        }
+      obj.forEach((arrayItem) => {
+        let x = arrayItem.content
+        // console.log(x)
+
+        let str1 = x['rendered']
+
+        console.log(str1)
+
+        // str1 = x.slice(13, -22)
+        output.innerHTML = str1
+      })
+    } else {
+      output.innerHTML = 'Error'
     }
-    xhr.open("GET", url);
-    xhr.send();
-    // console.log(xhr);
+  }
+  xhr.open('GET', url)
+  xhr.send()
+  // console.log(xhr);
 
-    xhr.addEventListener("progress", callBackfn);
-    xhr.addEventListener("load", callBackfn);
-    xhr.addEventListener("error", callBackfn);
+  xhr.addEventListener('progress', callBackfn)
+  xhr.addEventListener('load', callBackfn)
+  xhr.addEventListener('error', callBackfn)
 }
 
-function callBackfn(e){
-    console.log(e);
+function callBackfn(e) {
+  console.log(e)
 }
-
-
